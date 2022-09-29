bc71b9e257a14decc53b99b2d7e51a00dee6fba8
Dawid Kocik <kocik.dawid@wp.pl>
2022-09-29 21:28:16 +0200

    second example

---
+++ src/main/java/my/task/envelo/second/first/ExampleTwo.java
@@ -0,0 +1,24 @@
+package my.task.envelo.second.first;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ExampleTwo {
+    private static final List<Long> tribList = new ArrayList<>(Arrays.asList(0L, 1L, 2L));
+
+    public static Long calculateTribRecList(int n) {
+        if (n < 0)
+            throw new ArithmeticException("The number must be higher than 0" );
+        if (tribList.size() < n) {
+            Long value = calculateTribRecList(n - 3) + calculateTribRecList(n - 2) + calculateTribRecList(n - 1);
+            tribList.add(value);
+        }
+        return tribList.get(n - 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(        ExampleTwo.calculateTribRecList(1));
+
+    }
+}
