a67f36b157021ecbe958a7e1ebdb7dd174d32487
Dawid Kocik <kocik.dawid@wp.pl>
2022-10-02 20:20:22 +0200

    envelo task

---
+++ src/main/java/my/task/extra/API_Kanye_Rest/KanyeRest.java
@@ -0,0 +1,37 @@
+package my.task.extra.API_Kanye_Rest;
+
+import java.io.IOException;
+import java.net.*;
+import java.util.Scanner;
+
+public class KanyeRest {
+
+    private static boolean isUrlValid(String url) {
+        try {
+            URL obj = new URL(url);
+            obj.toURI();
+            return true;
+        } catch (MalformedURLException | URISyntaxException e) {
+            return false;
+        }
+    }
+
+    public static void getQuote() throws IOException {
+        String url = "https://api.kanye.rest/";
+        StringBuilder builder = new StringBuilder();
+        if(isUrlValid(url)) {
+            URL obj = new URL(url);
+            HttpURLConnection conn = (HttpURLConnection) obj.openConnection();
+            conn.setRequestMethod("GET");
+            int response = conn.getResponseCode();
+            if (response == 200) {
+                Scanner responseReader = new Scanner(conn.getInputStream());
+                while (responseReader.hasNext()) {
+                    builder.append(responseReader.nextLine());
+                }
+                responseReader.close();
+                System.err.println(builder);
+            }
+        }
+    }
+}
---
+++ src/main/java/my/task/extra/API_Kanye_Rest/Main.java
@@ -0,0 +1,9 @@
+package my.task.extra.API_Kanye_Rest;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        KanyeRest.getQuote();
+    }
+}
